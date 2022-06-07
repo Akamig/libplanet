ba4cf3810423222f2967d8915c376a177bb01202
Akamig <akamig@posteo.net>
2022-06-07 21:32:35 +0900

    Fix SQL Valid

--- Libplanet.Explorer/sql/initialize-rich-store.sql
+++ Libplanet.Explorer/sql/initialize-rich-store.sql
@@ -23,22 +23,21 @@ CREATE TABLE IF NOT EXISTS `updated_address_references` (
 );
 
 CREATE TABLE IF NOT EXISTS `blocks` (
-    CONSTRAINT `uid` UNIQUE (`hash`,)
+    CONSTRAINT `uid` UNIQUE (`hash`),
 
-    "difficulty"            BIGINT
-    "hash"                  VARCHAR(64)
-    "hash_algorithm"        VARCHAR(100)
-    "index"                 BIGINT
-    "miner"                 VARCHAR(42)
-    "nonce"                 VARCHAR(20)
-    "pre_evaluation_hash"
-    "previous_hash"         VARCHAR(64)
-    "protocol_version"      TINYINT UNSIGNED
-    "public_key"            VARCHAR(66)
-    "signature"
-    "state_root_hash"       VARCHAR(64)
-    "timestamp"             DATETIME(6)
-    "total_difficulty"      BIGINT
-    "tx_hash"               VARCHAR(64)
-
-)
+    `difficulty`            BIGINT,
+    `hash`                  VARCHAR(64),
+    `hash_algorithm`        VARCHAR(100),
+    `index`                 BIGINT,
+    `miner`                 VARCHAR(42),
+    `nonce`                 VARCHAR(20),
+    `pre_evaluation_hash`   BINARY(20),
+    `previous_hash`         VARCHAR(64),
+    `protocol_version`      TINYINT UNSIGNED,
+    `public_key`            VARCHAR(66),
+    `signature`             BINARY(20),
+    `state_root_hash`       VARCHAR(64),
+    `timestamp`             DATETIME(6),
+    `total_difficulty`      BIGINT,
+    `tx_hash`               VARCHAR(64)
+);
